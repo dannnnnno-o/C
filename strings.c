55f3f64c0ecc3ef2ccad32a09a5188d7f3c469e3
dannnnnn <dannnnnnnno.o@gmail.com>
2025-08-31 23:24:15 +0800

    some string functions from <string.h> library

---
+++ strings.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+
+    char string1[] = "Bro";
+    char string2[] = "Code";
+    int result;
+
+    //strlwr(string1);                                // converts strings to lowercase
+    //strupr(string1);                               // converts strings to uppercase
+    //strcat(string1, string2);                       // appends string2 into the end of string1
+    //strncat(string1, string2, 1);                   // appends an 'n' amount of characters where n = 3rd param
+    //strcpy(string1, string2);                       // copies string 2 to string 1 
+    //strncpy(string1, string2, 4);                   // copies an 'n' amount of characters from str2 to str1
+    //strset(string1, '?');                           // sets every character from a string to a given character
+    //strnset(string1, 'x', 1);                       // sets an 'n' amount of character to given char
+    //strrev(string1);                              // returns a reversed order of a given string;
+
+    //result = strlen(string1);                       // returns the length of a string
+    //result = strcmp(string1, string2);              // returns 0 if true, !0 if false
+    
+
+    
+    
+    printf("%d", result);
+
+
+    return 0;
+}
