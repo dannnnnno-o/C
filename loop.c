2799b9f8046923734cc0c7f2b22fdbc501d4c355
dannnnnn <dannnnnnnno.o@gmail.com>
2025-09-06 18:34:07 +0800

    nested loop

--- loop.c
+++ loop.c
@@ -1,16 +1,26 @@
 #include<stdio.h>
 
 int main(){
-    int i;
+/*     int i;
     int j;
     printf("How long do you want to loop?: ");
     scanf("%d", j);
 
     for(i = 0; i <= 5; i++){
         printf("%d\n", i);
+    } */
+
+    int i, j;
+    int n = 4;
+    
+    for (i = 1; i <= n; i++){
+        for (j = n; j >=1; j--){
+            if(i >= j)
+            printf("* ");
+            else
+            printf(" ");
+        }
+        printf("\n");
     }
-
-
-
-    return 0;
+return 0;
 }
